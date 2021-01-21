4566a92e93edf10bd5966d25841945c7d6d7002e
meu29 <meu29@ezweb.ne.jp>
2021-01-21 19:57:16 +0900

    crud単体テスト

--- tweet/tests.py
+++ tweet/tests.py
@@ -2,12 +2,28 @@ from django.test import TestCase
 from .models import Post
 
 class PostTestCace(TestCase):
- 
-    #メソッド名をtestから始めないと実行されない
-    def test_post_tweet(self):
-        content = "これはテストです"
-        Post(content = content, author = "test_bot").save()
-        newTweet = Post.objects.filter(content__contains = content).values()[0]
-        return self.assertEqual(content, newTweet["content"])
 
+    content = "これはテストです"
+
+    #メソッド名をtestから始めないと実行されない
+    #各メソッドの終了時にテーブルの内容は消える(?)
+
+    def test_post_tweet(self):
+        Post(content = PostTestCace.content, author = "test_bot").save()
+        newTweet = Post.objects.filter(content__contains = PostTestCace.content).values()[0]
+        return self.assertEqual(PostTestCace.content, newTweet["content"])
+
+    def test_update_tweet(self):
+        Post(content = PostTestCace.content, author = "test_bot").save()
+        newTweet = Post.objects.filter(content__contains = PostTestCace.content).values()[0]
+        Post.objects.filter(content__contains = PostTestCace.content).update(likes = 1)
+        newTweet = Post.objects.filter(content__contains = PostTestCace.content).values()[0]
+        return self.assertEqual(1, newTweet["likes"])
+    
+    def test_delete_tweet(self):
+        Post(content = PostTestCace.content, author = "test_bot").save()
+        newTweet = Post.objects.filter(content__contains = PostTestCace.content).values()[0]
+        Post.objects.filter(content__contains = PostTestCace.content).delete()
+        newTweet = Post.objects.filter(content__contains = PostTestCace.content).values()
+        return self.assertEqual(0, len(newTweet))
 
